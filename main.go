c0da2b56a8d9fb634c264c4c00a92ad762fed183
Rick Friedrich <rick@hstdesigns.de>
2020-05-06 15:10:47 +0200

    print goreleaser build flags on start

--- main.go
+++ main.go
@@ -1,12 +1,22 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
 
 	"github.com/labstack/echo/v4"
 )
 
+var (
+	version = "dev"
+	commit  = "none"
+	date    = "unknown"
+	builtBy = "unknown"
+)
+
 func main() {
+	fmt.Printf("my app %s, commit %s, built at %s by %s", version, commit, date, builtBy)
+
 	e := echo.New()
 	e.GET("/", func(c echo.Context) error {
 		return c.String(http.StatusOK, "Hello, World!")
